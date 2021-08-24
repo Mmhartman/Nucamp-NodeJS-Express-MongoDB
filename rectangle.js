521b649f213afde4808b472b880bc92bfde40bbe
Mmhartman <83313908+Mmhartman@users.noreply.github.com>
2021-08-24 12:06:29 -0700

    Node Callbacks and Error Handling

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,14 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+module.exports = (x, y, callback) =>  {
+      if (x <= 0 || y <= 0) {
+        //ERROR CALL BACK PATTERN. STNDARD PRACTICE IN NODE.JS FOR ASYNC FUNC TO RETURN AN ERROR OBJ AS THE 1ST ARGUMNT //
+        callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
+    } else {
+        setTimeout(() => // error function that contains the callback 
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area:() => x * y
+            }),
+            2000 // MS to wait // 
+            );
+        }
+    };
